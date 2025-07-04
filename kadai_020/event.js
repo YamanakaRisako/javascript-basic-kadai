dac34bd3a7a421274a1ef57ad963c8af84c130ef
YamanakaRisako <hello19930810@gmail.com>
2025-07-04 17:42:14 +0900

    kadai_020をアップロードしました

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+const tx=document.getElementById('text');
+
+const btn=document.getElementById('btn');
+
+btn.addEventListener('click',()=>{
+   
+    tx.textContent='ボタンをクリックしました'
+
+
+
+    
+});
+    
